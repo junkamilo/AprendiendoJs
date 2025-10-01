53713b54a080c3c01bfdb10bb422d9c2667b6d80
Juan camilo <juancamilo.campobarrios@gmail.com>
2025-10-01 11:07:37 -0500

    aprendiendo js

---
+++ NiverlJunior/introduccion/js/variables.js
@@ -0,0 +1,43 @@
+//formas de declarar las variables
+var variableVar = 22
+//esta variable no permite cambiar su valor y se tiene que declarar y inicializar.
+const variableConst = "Const"
+//declaramos y inicializamos la variable al mismo tiempo.
+let variableLet = true;
+
+//declaramos una variables
+let declarar;
+
+//declramos y inicializamos varias variables
+let numero1 = 20, numero = 2; numero = 4;
+
+//inicializamos la variable
+declarar = 29;
+console.log(declarar);
+
+//variable vacia
+const variableVacia = null;
+
+//variable NaN
+const number1 = 20;
+const number2 = "Juan";
+console.log(number1*number2);
+
+
+
+//tipos de datos de las variables
+string = "cadena de texto";
+number = 22
+booleano = true || false;
+
+//casos especiales de las variables
+/**-undefine: se muestra cuando declaramos una variable sin inicializar.
+ * -Null: cuando queremos una variable pero que este vacia se le aplica null.
+ * -Nan: cuando se multiplica variable numero con variable tipo string o diferente.
+*/ 
+
+//promt ingreso de valores de los usuarios
+
+//almacenamos el nombre del usuario en la variable nombre
+let nombre = prompt("Ingresa tu nombre");
+console.log("Hola Bienvenido " + nombre);
